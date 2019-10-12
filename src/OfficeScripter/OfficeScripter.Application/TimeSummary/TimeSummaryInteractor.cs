209df28234e1ff9245970bb397d3f2a0e3545673
Łukasz Soleski <luk.soleski@gmail.com>
2019-10-12 20:41:14 +0200

    Create model for the time summary events.

---
+++ src/OfficeScripter/OfficeScripter.Application/TimeSummary/TimeSummaryInteractor.cs
@@ -0,0 +1,12 @@
+﻿using OfficeScripter.Abstractions.TimeSummary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeScripter.Application.TimeSummary
+{
+    public class TimeSummaryInteractor: ITimeSummary
+    {
+        
+    }
+}
