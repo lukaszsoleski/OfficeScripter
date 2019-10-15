ba3d8bd484f74611a0a27767c03e611b76f0d2a1
Łukasz Soleski <luk.soleski@gmail.com>
2019-10-15 09:55:52 +0200

    [WokTime|Add] Sample data for testing purposes

---
+++ src/OfficeScripter/OfficeScripter.Persistance/Initializer/TimeSummary/TimeSummaryInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Itenso.TimePeriod;
+using OfficeScripter.Domain.TimeSummary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeScripter.Persistance.Initializer.TimeSummary
+{
+    public class TimeSummaryInitializer
+    {
+       
+    }
+}
--- src/OfficeScripter/OfficeScripter.UnitTest/TimeSummary/TimeSummaryInteractorTests.cs
+++ src/OfficeScripter/OfficeScripter.UnitTest/TimeSummary/TimeSummaryInteractorTests.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Documents;
 using Xunit;
 
 namespace OfficeScripter.UnitTest.TimeSummary
@@ -24,31 +25,81 @@ namespace OfficeScripter.UnitTest.TimeSummary
         public void GetTimeSummary_SumWorkTimeInTheGivenTimeBlock()
         {
             var mocker = new AutoMocker();
-            var startDate = new DateTime(2019, 10, 1, 8,0,0);
             var hoursToAdd = 8;
+            var startDate = new DateTime(2019, 10, 2, 8, 0, 0);
+
+            var timeBlock = new TimeBlock();
+            var interactor = mocker.CreateInstance<TimeSummaryInteractor>();
+            var timePeriod = TimePeriodTypeEnum.Month;
+            var events = SampleTestData(startDate);
+            // act
+            var summary = interactor.GetTimeSummary(events, timeBlock, timePeriod);
+            // assert
+            summary.TimeSpan.TotalHours.Should().Be(8);
+        }
+        /// <summary>
+        /// Sample data for testing purposes.
+        /// </summary>
+        private List<TimeSummaryEvent> SampleTestData(DateTime startDate)
+        {
+
+
             var events = new List<TimeSummaryEvent>()
             {
+                // Work start + 0:00
                 new TimeSummaryEvent()
                 {
                     CreatedAt = startDate,
                     EventType = EventTypeEnum.WorkStart,
                     ProjectType = ProjectTypeEnum.Unknown
                 },
+                // Work break start +4:00  
                 new TimeSummaryEvent()
                 {
-                    CreatedAt = startDate.AddHours(hoursToAdd),
+                    CreatedAt = startDate.AddHours(4), 
+                    EventType = EventTypeEnum.WorkBreakStart,
+                    ProjectType = ProjectTypeEnum.Unknown
+                },
+                // Work break end +6:30  
+                new TimeSummaryEvent()
+                {
+                    CreatedAt = startDate.AddHours(6).AddMinutes(30),
+                    EventType = EventTypeEnum.WorkBreakEnd,
+                    ProjectType = ProjectTypeEnum.Unknown
+                },
+                // Work end +8:00           -- 5:30 of the work time
+                new TimeSummaryEvent()
+                {
+                    CreatedAt = startDate.AddHours(8), 
                     EventType = EventTypeEnum.WorkEnd,
                     ProjectType = ProjectTypeEnum.Unknown
-                }
+                },
+                // Next day work start +24:00
+                 new TimeSummaryEvent()
+                {
+                    CreatedAt = startDate.AddDays(1), 
+                    EventType = EventTypeEnum.WorkStart,
+                    ProjectType = ProjectTypeEnum.Unknown
+                },
+                 // English lesson +25:00
+                   new TimeSummaryEvent()
+                {
+                    CreatedAt = startDate.AddDays(1).AddHours(1),
+                    EventType = EventTypeEnum.Unknown,
+                    ProjectType  = ProjectTypeEnum.EnglishLesson
+                },
+                 // Work end +31:00         -- 7h - enLesson(1.5h default)
+                new TimeSummaryEvent()
+                {
+                    CreatedAt = startDate.AddDays(1).AddHours(7).AddMinutes(15),
+                    EventType = EventTypeEnum.WorkEnd,
+                    ProjectType = ProjectTypeEnum.Unknown
+                },
+                
+                // sum ~= 11h15min               
             };
-            var timeBlock = new TimeBlock();
-            var interactor = mocker.CreateInstance<TimeSummaryInteractor>();
-            var timePeriod = TimePeriodTypeEnum.Month;
+            return events;
 
-            // act
-            var summary = interactor.GetTimeSummary(events, timeBlock, timePeriod);
-            // assert
-            summary.TimeSpan.TotalHours.Should().Be(8);
         }
 
     }
